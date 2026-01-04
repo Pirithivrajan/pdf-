c48a9cac95eb2ed45a1ce41abddda87896becb8f
Pirithivrajan <pirithivrajanselva@gmail.com>
2026-01-04 08:59:34 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    req.user = jwt.verify(token, "SECRET");
+    req.user = jwt.verify(token, "SECRET_KEY");
     next();
   } catch {
-    res.status(401).send("Unauthorized");
+    res.status(401).json({ message: "Unauthorized" });
   }
 };
