d7e49267a384cce133248378e2769aac4543b00c
Pirithivrajan <pirithivrajanselva@gmail.com>
2026-01-04 08:57:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
-module.exports = () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/pdf_saas");
+module.exports = async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/pdf_saas", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log("MongoDB connected");
 };
