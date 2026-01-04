ebb41b044fb3dc70806dc11ce5b60b76d030356e
Pirithivrajan <pirithivrajanselva@gmail.com>
2026-01-04 08:50:38 +0530

    Create auth2.js

---
+++ auth2.js
@@ -0,0 +1,14 @@
+async function login() {
+  const res = await fetch("http://localhost:3000/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value
+    })
+  });
+
+  const data = await res.json();
+  localStorage.token = data.token;
+  location.href = "dashboard.html";
+}
