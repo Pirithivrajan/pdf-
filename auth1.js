738a61394bca11c3061ca6fc82b85fc829831bb5
Pirithivrajan <pirithivrajanselva@gmail.com>
2026-01-04 08:59:51 +0530

    Update auth1.js

--- auth1.js
+++ auth1.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   await new User(req.body).save();
-  res.send("OK");
+  res.json({ message: "Registered" });
 });
 
 router.post("/login", async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("No user");
+  if (!user) return res.status(400).json({ message: "User not found" });
 
   const ok = await bcrypt.compare(req.body.password, user.password);
-  if (!ok) return res.status(400).send("Wrong password");
+  if (!ok) return res.status(400).json({ message: "Wrong password" });
 
-  const token = jwt.sign({ id: user._id }, "SECRET");
+  const token = jwt.sign({ id: user._id }, "SECRET_KEY");
   res.json({ token });
 });
 
